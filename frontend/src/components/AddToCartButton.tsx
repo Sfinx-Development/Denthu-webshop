b041cbd69371cfa7b2f5453cc9767a076bfaf4d9
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-08-26 21:35:02 +0200

    bas för första sidan

---
+++ frontend/src/components/AddToCartButton.tsx
@@ -0,0 +1,68 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Button, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Product } from "../slices/productSlice";
+import { useAppDispatch } from "../slices/store";
+import { addToCart } from "../slices/cartSlice";
+
+interface Props {
+  product: Product;
+}
+
+const AddtoCartButton: React.FC<Props> = ({ product }) => {
+  const [productAddedToCart, setProductAddedToCart] = useState(false);
+  const [resetButton, setResetButton] = useState(false);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (resetButton) {
+      setProductAddedToCart(false);
+    }
+  }, [resetButton]);
+
+  const handleAddToCart = () => {
+    if (!productAddedToCart) {
+      dispatch(addToCart(product));
+      setProductAddedToCart(true);
+      setTimeout(() => {
+        setResetButton(true);
+        setTimeout(() => {
+          setResetButton(false);
+        });
+      }, 1000);
+    }
+  };
+
+  return (
+    <div>
+      <Button
+        data-cy="product-buy-button"
+        onClick={handleAddToCart}
+        variant="contained"
+        disabled={productAddedToCart || resetButton}
+        sx={{
+          mt: 1,
+          mb: 1,
+          background: "#000",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "black",
+            transform: "scale(1.05)",
+            transition: "transform 0.2s ease-in-out",
+          },
+        }}
+      >
+        <ShoppingCartIcon fontSize="small" style={{ marginRight: "8px" }} />
+        {productAddedToCart ? (
+          <Typography variant="body1" data-cy="added-to-cart-toast">
+            har lagts till
+          </Typography>
+        ) : (
+          <Typography variant="body1">Lägg till</Typography>
+        )}
+      </Button>
+    </div>
+  );
+};
+
+export default AddtoCartButton;
