bb1c09f255b8f4541e519dfa08efb5c82bcfff6a
Angelina Holmqvist <angelinaholmqvist@live.se>
2024-08-28 19:54:08 +0200

    tog bort product typen

--- frontend/src/pages/CategoryProducts.tsx
+++ frontend/src/pages/CategoryProducts.tsx
@@ -1,18 +1,9 @@
+import { Box, Card, CardMedia, Grid, Typography } from "@mui/material";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Box, Grid, Typography, Card, CardMedia } from "@mui/material";
-import { useParams } from "react-router-dom";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { Link } from "react-router-dom"; 
+import { Link, useParams } from "react-router-dom";
 import { db } from "../api/config";
-
-// import { db } from "../firebaseConfig"; // Se till att detta är rätt importväg till din firebaseConfig
-
-interface Product {
-  id: string;
-  name: string;
-  imageUrl: string;
-  category: string;
-}
+import { Product } from "../slices/productSlice";
 
 export default function CategoryProducts() {
   const { categoryName } = useParams<{ categoryName: string }>();
@@ -25,7 +16,7 @@ export default function CategoryProducts() {
         // Query som hämtar produkter som tillhör en viss kategori
         const q = query(
           collection(db, "products"),
-          where("category", "==", categoryName)  // Filtrera på kategori
+          where("category", "==", categoryName) // Filtrera på kategori
         );
         const querySnapshot = await getDocs(q);
         const fetchedProducts: Product[] = [];
@@ -56,62 +47,67 @@ export default function CategoryProducts() {
       <Typography
         variant="h4"
         component="h1"
-        sx={{ marginBottom: 4, textTransform: "uppercase", textAlign: "center" }}
+        sx={{
+          marginBottom: 4,
+          textTransform: "uppercase",
+          textAlign: "center",
+        }}
       >
         {categoryName}
       </Typography>
       <Grid container spacing={4}>
         {products.map((product) => (
           <Grid item xs={12} sm={6} md={4} key={product.id}>
-               <Link to={`/product/${product.id}`} style={{ textDecoration: "none" }}>
-            <Card
-              sx={{
-                boxShadow: "0px 10px 20px rgba(0,0,0,0.1)",
-                borderRadius: "16px",
-                overflow: "hidden",
-                backgroundColor: "#fff",
-                "&:hover": {
-                  boxShadow: "0px 20px 30px rgba(0,0,0,0.15)",
-                },
-              }}
+            <Link
+              to={`/product/${product.id}`}
+              style={{ textDecoration: "none" }}
             >
-              <CardMedia
-                component="img"
-                alt={product.name}
-                image={product.imageUrl}
+              <Card
                 sx={{
-                  height: { xs: "200px", md: "300px" },
-                  objectFit: "contain",
-                  transition: "transform 0.4s ease",
+                  boxShadow: "0px 10px 20px rgba(0,0,0,0.1)",
+                  borderRadius: "16px",
+                  overflow: "hidden",
+                  backgroundColor: "#fff",
                   "&:hover": {
-                    transform: "scale(1.1)",
+                    boxShadow: "0px 20px 30px rgba(0,0,0,0.15)",
                   },
                 }}
-              />
-              <Box sx={{ padding: 3 }}>
-                <Typography
-                  variant="h6"
-                  component="div"
+              >
+                <CardMedia
+                  component="img"
+                  alt={product.name}
+                  image={product.imageUrl}
                   sx={{
-                    color: "#333",
-                    fontWeight: "bold",
-                    textTransform: "uppercase",
-                    fontSize: { xs: "1rem", md: "1.1rem" },
-                    letterSpacing: 0.5,
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
+                    height: { xs: "200px", md: "300px" },
+                    objectFit: "contain",
+                    transition: "transform 0.4s ease",
+                    "&:hover": {
+                      transform: "scale(1.1)",
+                    },
                   }}
-                >
-                  {product.name}
-                </Typography>
-              </Box>
-            </Card>
+                />
+                <Box sx={{ padding: 3 }}>
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{
+                      color: "#333",
+                      fontWeight: "bold",
+                      textTransform: "uppercase",
+                      fontSize: { xs: "1rem", md: "1.1rem" },
+                      letterSpacing: 0.5,
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                    }}
+                  >
+                    {product.name}
+                  </Typography>
+                </Box>
+              </Card>
             </Link>
           </Grid>
-          
         ))}
-        
       </Grid>
     </Box>
   );
