34d0d91531a1d8dd4edfc963d43809a8877c460c
Elina Kerola <elinak90@icloud.com>
2024-09-11 13:41:18 +0200

    navigate till cart

---
+++ frontend/src/pages/Cart.tsx
@@ -0,0 +1,171 @@
+
+import { Box, Typography } from "@mui/material";
+
+export default function Cart() {
+  return (
+    <Box>
+      <Typography>Cart</Typography>
+    </Box>
+  );
+}
+
+
+// // import AddCircleIcon from "@mui/icons-material/AddCircle";
+// // import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+// import { Box, Typography } from "@mui/material";
+// // import ListComponent from "../components/ListComponent";
+// // import { useCart } from "../contexts/CartContext";
+
+// export default function CartPage() {
+//   const { cart, totalPrice } = useCart();
+
+//   return (
+//     <Box
+//       display={"flex"}
+//       flexDirection={"column"}
+//       justifyContent={"space-around"}
+//       alignContent={"center"}
+//       width={"80%"}
+//     >
+//       {cart ? (
+//         <Box>
+//           {/* <ListComponent */}
+//             {/* products={cart.map((p) => ({
+//               ...p,
+//               customButtons: [
+//                 {
+//                   icon: <AddCircleIcon />,
+//                   onClick: () => addToCart(p),
+//                   datacy: "increase-quantity-button",
+//                 },
+//                 {
+//                   icon: <RemoveCircleIcon />,
+//                   onClick: () => removeFromCart(p),
+//                   datacy: "decrease-quantity-button",
+//                 },
+//               ],
+//             }))} */}
+//           {/* /> */}
+
+//           <Typography
+//             variant="h6"
+//             display={"flex"}
+//             justifyContent={"space-between"}
+//             mx={2}
+//           >
+//             <Typography>Totalt pris </Typography>
+//             <Typography data-cy="total-price">{totalPrice} kr</Typography>
+//           </Typography>
+//         </Box>
+//       ) : (
+//         <Typography variant="body1">Din varukorg är tom</Typography>
+//       )}
+//     </Box>
+//   );
+// }
+
+
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// function useCart(): { cart: any; totalPrice: any; addToCart: any; removeFromCart: any; } {
+//     throw new Error("Function not implemented.");
+// }
+// import { Box, Card, CardMedia, Grid, Typography } from "@mui/material";
+// import { useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import { useAppDispatch, useAppSelector } from "../slices/store";
+// import { getProductsByCategoryAsync } from "../slices/productSlice";
+
+// export default function CategoryProducts(){
+//   const { id } = useParams<{ id: string }>();
+//   const dispatch = useAppDispatch();
+//   const filteredProducts = useAppSelector(
+//     (state) => state.productSlice.filteredProducts
+//   );
+//   const loading = useAppSelector((state) => state.productSlice.loading);
+
+//   useEffect(() => {
+//     console.log("ID: ", id);
+//     if (id) {
+//       dispatch(getProductsByCategoryAsync(id)); // Anropar thunk för att hämta produkter baserat på kategori
+//     }
+//   }, [id]);
+
+//   if (loading) {
+//     return <Typography>Loading...</Typography>;
+//   }
+
+//   if (filteredProducts.length === 0) {
+//     return <Typography>No products found for this category.</Typography>;
+//   }
+
+//   return (
+//     <Box sx={{ width: "100%", padding: 4, backgroundColor: "#f4f4f4" }}>
+//       <Typography
+//         variant="h4"
+//         component="h1"
+//         sx={{
+//           marginBottom: 4,
+//           textTransform: "uppercase",
+//           textAlign: "center",
+//         }}
+//       >
+//         {id}
+//       </Typography>
+//       <Grid container spacing={4}>
+//         {filteredProducts.map((product) => (
+//           <Grid item xs={12} sm={6} md={4} key={product.id}>
+//             <Link
+//               to={`/product/${product.id}`}
+//               style={{ textDecoration: "none" }}
+//             >
+//               <Card
+//                 sx={{
+//                   boxShadow: "0px 10px 20px rgba(0,0,0,0.1)",
+//                   borderRadius: "16px",
+//                   overflow: "hidden",
+//                   backgroundColor: "#fff",
+//                   "&:hover": {
+//                     boxShadow: "0px 20px 30px rgba(0,0,0,0.15)",
+//                   },
+//                 }}
+//               >
+//                 <CardMedia
+//                   component="img"
+//                   alt={product.name}
+//                   image={product.imageUrl}
+//                   sx={{
+//                     height: { xs: "200px", md: "300px" },
+//                     objectFit: "contain",
+//                     transition: "transform 0.4s ease",
+//                     "&:hover": {
+//                       transform: "scale(1.1)",
+//                     },
+//                   }}
+//                 />
+//                 <Box sx={{ padding: 3 }}>
+//                   <Typography
+//                     variant="h6"
+//                     component="div"
+//                     sx={{
+//                       color: "#333",
+//                       fontWeight: "bold",
+//                       textTransform: "uppercase",
+//                       fontSize: { xs: "1rem", md: "1.1rem" },
+//                       letterSpacing: 0.5,
+//                       whiteSpace: "nowrap",
+//                       overflow: "hidden",
+//                       textOverflow: "ellipsis",
+//                     }}
+//                   >
+//                     {product.name}
+//                   </Typography>
+//                 </Box>
+//               </Card>
+//             </Link>
+//           </Grid>
+//         ))}
+//       </Grid>
+//     </Box>
+//   ); 
+// }
