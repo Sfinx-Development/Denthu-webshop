83738b5f349363320501d97bb905c450a55ca7e2
Elina Kerola <elinak90@icloud.com>
2024-10-23 15:07:16 +0200

    spalt hanterade ordrar

--- frontend/src/pages/OrdersForShipping.tsx
+++ frontend/src/pages/OrdersForShipping.tsx
@@ -19,9 +19,10 @@ export default function OrdersForShipping() {
   const orders = useAppSelector((state) => state.orderSlice.orders);
   const [shippingOrders, setShippingOrders] = useState<Order[]>([]);
   const [pickupOrders, setPickupOrders] = useState<Order[]>([]);
-  const [selectedOrders, setSelectedOrders] = useState<{
-    [key: string]: boolean;
-  }>({});
+  const [handledOrders, setHandledOrders] = useState<Order[]>([]); 
+  // const [selectedOrders, setSelectedOrders] = useState<{
+  //   [key: string]: boolean;
+  // }>({});
   const products = useAppSelector((state) => state.productSlice.products);
 
   useEffect(() => {
@@ -38,8 +39,11 @@ export default function OrdersForShipping() {
       const pickup = orders.filter(
         (order) => order.shippingMethod === "pickup" && order.status !== "Paid"
       );
+       // Lägg till logik för hanterade ordrar här (exempelvis de som har status 'Handled')
+       const handled = orders.filter(order => order.status === "Handled"); // Exempel
       setShippingOrders(shipping);
       setPickupOrders(pickup);
+      setHandledOrders(handled)
     }
   }, [orders]);
 
@@ -51,7 +55,7 @@ export default function OrdersForShipping() {
     <Box display="flex" flexDirection="column" height="100vh">
       <Box flexGrow={1} p={2} overflow="auto">
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={4}>
             <Typography variant="h4" gutterBottom>
               Ordrar för frakt
             </Typography>
@@ -119,7 +123,7 @@ export default function OrdersForShipping() {
             </List>
           </Grid>
 
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={4}>
             <Typography variant="h4" gutterBottom>
               Ordrar för upphämtning
             </Typography>
@@ -136,6 +140,72 @@ export default function OrdersForShipping() {
                     sx={{ cursor: "pointer" }}
                     onClick={() => navigate(`/admin/orderdetail/${order.id}`)}
                 
+                  >
+                    <ListItemText
+                      primary={`Order ID: ${order.id}`}
+                      secondary={
+                        <Box>
+                          <Typography variant="body2">
+                            Kund: {order.guestFirstName} {order.guestLastName}
+                          </Typography>
+                          <Typography variant="body2">
+                            Telefon: {order.guestPhone}
+                          </Typography>
+                          <Typography variant="body2">
+                            E-post: {order.guestEmail}
+                          </Typography>
+                        </Box>
+                      }
+                    />
+                    <ListSubheader>Produkter:</ListSubheader>
+                    <List>
+                      {order.items &&
+                        order.items.map((item) => {
+                          const product = products.find(
+                            (p) => p.id === item.product_id
+                          );
+                          return (
+                            <ListItem key={item.id}>
+                              <ListItemText
+                                primary={`${
+                                  product ? product.name : "Produkt okänd"
+                                } (Antal: ${item.quantity})`}
+                                secondary={
+                                  <Box>
+                                    <Typography variant="body2">
+                                      Pris: {item.price / 100} SEK
+                                    </Typography>
+                                    <Typography variant="body2">
+                                      Total summa:{" "}
+                                      {(item.quantity * item.price) / 100} kr
+                                    </Typography>
+                                  </Box>
+                                }
+                              />
+                            </ListItem>
+                          );
+                        })}
+                    </List>
+                  </ListItem>
+                ))}
+            </List>
+          </Grid>
+          <Grid item xs={12} sm={4}> {/* Ny kolumn för hanterade ordrar */}
+            <Typography variant="h4" gutterBottom>
+              Hanterade Ordrar
+            </Typography>
+            <List>
+              {handledOrders
+                .sort(
+                  (a, b) =>
+                    new Date(b.created_date).getTime() -
+                    new Date(a.created_date).getTime()
+                )
+                .map((order) => (
+                  <ListItem
+                    key={order.id}
+                    sx={{ cursor: "pointer" }}
+                    onClick={() => navigate(`/admin/orderdetail/${order.id}`)}
                   >
                     <ListItemText
                       primary={`Order ID: ${order.id}`}
