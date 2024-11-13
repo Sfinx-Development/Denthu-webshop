d4857f56f62aba218621d0e77ce03c47107b1986
Elina Kerola <elinak90@icloud.com>
2024-11-13 20:05:53 +0100

    hdfhsdjsklfsdf

--- frontend/src/api/swedbank.ts
+++ frontend/src/api/swedbank.ts
@@ -17,7 +17,7 @@ export async function PostPaymentOrder(paymentOrder: PaymentOrderOutgoing) {
   return fetch(uri, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json;version=3.1;charset=utf-8",
+      "Content-Type": "application/json; charset=utf-8; version=3.1",
       Authorization: `Bearer ${bearer}`,
       Host: "api.externalintegration.payex.com",
     },
